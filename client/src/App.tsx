b0a579d8cc49cc63531333e20d31ec160d5055a4
Alasela Babatunde <alaselababatunde10@gmail.com>
2025-11-05 13:06:47 +0100

    Updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './context/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
 
 // Pages
 import Login from './pages/Login';
